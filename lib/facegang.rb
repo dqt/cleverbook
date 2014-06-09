0d4214f14f81938f87f1c8d92dd9076934db3fbe
Thomas Gentry <thomas.gentry@answernet.com>
2014-06-09 13:29:24 -0400

    Fix instantiate Facegang::ChatBot

--- lib/facegang.rb
+++ lib/facegang.rb
@@ -3,6 +3,7 @@ require "facegang/facebook_macros"
 require "facegang/autoresponder"
 require "facegang/script/aiml"
 require "facegang/script/responser"
+require "facegang/chat_bot"
 
 module Facegang
   # Code meh
