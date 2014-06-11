74a3a4c70d3944d2d6e29f4232879fb562486f2a
Thomas Gentry <thomas.gentry@answernet.com>
2014-06-11 18:03:23 -0400

    Add scripting options

--- lib/cleverbook.rb
+++ lib/cleverbook.rb
@@ -11,8 +11,10 @@ module Cleverbook
     include Methadone::CLILogging
     include Cleverbook
     
+    # Factor out the app install into it's own tool
+
     def install_app_start_cb_autoresponder(config)
-    # Install facebook app and start autoresponder
+    # Install facebook app and start cleverbot autoresponder
       begin
         # Install facebook app, get access token to start responder
         info "Installing facebook app and getting token"
@@ -38,7 +40,7 @@ module Cleverbook
     end
 
     def start_cb_autoresponder(config)
-    # Skip app install, just get access token and start responder
+    # Skip app install, just get access token and start cleverbot responder
       begin
         # Install facebook app, get access token to start responder
         info "Installing facebook app and getting token"
@@ -58,6 +60,58 @@ module Cleverbook
         @ar.build_client
         # Run message call back thread
         @ar.run_bot_only_response_thread
+      rescue => e
+        error "#{e}"
+      end
+    end
+
+    def install_app_start_scripted_autoresponder(config)
+    # Install facebook app and start cleverbot autoresponder
+      begin
+        # Install facebook app, get access token to start responder
+        info "Installing facebook app and getting token"
+        @fb = FacebookMacros.new(config)
+        @fb.install_app_get_token
+        info "ACCESS TOKEN GRANTED: #{@fb.session[:access_token]}"
+
+        # Use Graph API to get our users profile
+        info "Polling Graph API for profile data"
+        @fb.get_graph
+        @profile = @fb.get_my_profile
+        debug "PROFILE: #{@profile.to_s}"
+        info "Starting autoresponder..."
+
+        # Build new client
+        @ar = Autoresponder.new(@profile, config["app_id"], @fb.session[:access_token], config["app_secret"])
+        @ar.build_client
+        # Run message call back thread
+        @ar.run_script_bot_mix_response_thread
+      rescue => e
+        error "#{e}"
+      end
+    end
+
+    def start_scripted_autoresponder(config)
+    # Skip app install, just get access token and start cleverbot responder
+      begin
+        # Install facebook app, get access token to start responder
+        info "Installing facebook app and getting token"
+        @fb = FacebookMacros.new(config)
+        @fb.already_installed_get_token
+        info "ACCESS TOKEN GRANTED: #{@fb.session[:access_token]}"
+
+        # Use Graph API to get our users profile
+        info "Polling Graph API for profile data"
+        @fb.get_graph
+        @profile = @fb.get_my_profile
+        debug "PROFILE: #{@profile.to_s}"
+        info "Starting autoresponder..."
+
+        # Build new client
+        @ar = Autoresponder.new(@profile, config["app_id"], @fb.session[:access_token], config["app_secret"])
+        @ar.build_client
+        # Run message call back thread
+        @ar.run_script_bot_mix_response_thread
       rescue => e
         error "#{e}"
       end
