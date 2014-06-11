42a6c18ebd2df0c9956cb37bd9de3420e2ce4e27
Thomas Gentry <thomas.gentry@answernet.com>
2014-06-11 16:32:45 -0400

    Module inheritance to self

--- lib/cleverbook.rb
+++ lib/cleverbook.rb
@@ -6,53 +6,55 @@ require "cleverbook/script/responser"
 require "cleverbook/chat_bot"
 
 module Cleverbook
-  def install_app_start_cb_autoresponder(config)
-  # Install facebook app and start autoresponder
-    begin
-      # Install facebook app, get access token to start responder
-      info "Installing facebook app and getting token"
-      @fb = FacebookMacros.new(config)
-      @fb.install_app_get_token
-      info "ACCESS TOKEN GRANTED: #{@fb.session[:access_token]}"
+  class << self
+    def install_app_start_cb_autoresponder(config)
+    # Install facebook app and start autoresponder
+      begin
+        # Install facebook app, get access token to start responder
+        info "Installing facebook app and getting token"
+        @fb = FacebookMacros.new(config)
+        @fb.install_app_get_token
+        info "ACCESS TOKEN GRANTED: #{@fb.session[:access_token]}"
 
-      # Use Graph API to get our users profile
-      info "Polling Graph API for profile data"
-      @fb.get_graph
-      @profile = @fb.get_my_profile
-      debug "PROFILE: #{@profile.to_s}"
-      info "Starting autoresponder..."
+        # Use Graph API to get our users profile
+        info "Polling Graph API for profile data"
+        @fb.get_graph
+        @profile = @fb.get_my_profile
+        debug "PROFILE: #{@profile.to_s}"
+        info "Starting autoresponder..."
 
-      # Build new client
-      client = Autoresponder.build_client(@profile["id"], config["app_id"], @fb.session[:access_token], config["app_secret"])
-      # Run message call back thread
-      Autoresponder.run_bot_only_response_thread(client, @profile["id"])
-    rescue => e
-      error "#{e}"
+        # Build new client
+        client = Autoresponder.build_client(@profile["id"], config["app_id"], @fb.session[:access_token], config["app_secret"])
+        # Run message call back thread
+        Autoresponder.run_bot_only_response_thread(client, @profile["id"])
+      rescue => e
+        error "#{e}"
+      end
     end
-  end
 
-  def start_cb_autoresponder(config)
-  # Skip app install, just get access token and start responder
-    begin
-      # Install facebook app, get access token to start responder
-      info "Installing facebook app and getting token"
-      @fb = FacebookMacros.new(config)
-      @fb.already_installed_get_token
-      info "ACCESS TOKEN GRANTED: #{@fb.session[:access_token]}"
+    def start_cb_autoresponder(config)
+    # Skip app install, just get access token and start responder
+      begin
+        # Install facebook app, get access token to start responder
+        info "Installing facebook app and getting token"
+        @fb = FacebookMacros.new(config)
+        @fb.already_installed_get_token
+        info "ACCESS TOKEN GRANTED: #{@fb.session[:access_token]}"
 
-      # Use Graph API to get our users profile
-      info "Polling Graph API for profile data"
-      @fb.get_graph
-      @profile = @fb.get_my_profile
-      debug "PROFILE: #{@profile.to_s}"
-      info "Starting autoresponder..."
+        # Use Graph API to get our users profile
+        info "Polling Graph API for profile data"
+        @fb.get_graph
+        @profile = @fb.get_my_profile
+        debug "PROFILE: #{@profile.to_s}"
+        info "Starting autoresponder..."
 
-      # Build new client
-      client = Autoresponder.build_client(@profile["id"], config["app_id"], @fb.session[:access_token], config["app_secret"])
-      # Run message call back thread
-      Autoresponder.run_bot_only_response_thread(client, @profile["id"])
-    rescue => e
-      error "#{e}"
+        # Build new client
+        client = Autoresponder.build_client(@profile["id"], config["app_id"], @fb.session[:access_token], config["app_secret"])
+        # Run message call back thread
+        Autoresponder.run_bot_only_response_thread(client, @profile["id"])
+      rescue => e
+        error "#{e}"
+      end
     end
   end
 end
